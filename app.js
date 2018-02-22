7813359d57c1d293d3e6d363c6d8f2c4a50b1902
BLACKBERRY\hoaiduy2503 <hoaiduy2503@gmail.com>
2018-02-22 17:30:00 +0700

    remover Accept header

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(body.json());
 
 app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
     next();
 });
 app.use(express.static(path.join(__dirname, '.')));
